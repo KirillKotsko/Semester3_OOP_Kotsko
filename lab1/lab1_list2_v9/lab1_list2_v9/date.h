afe3e362540bc1b368ac3170f838a4803a89db52
KirillKotsko <kerchickd@gmail.com>
2020-10-04 15:34:15 +0300

    Create class Time_is_now and (support) struct Date

--- lab1/lab1_list2_v9/lab1_list2_v9/date.h
+++ lab1/lab1_list2_v9/lab1_list2_v9/date.h
@@ -1,1 +1,34 @@
 #pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <ctime>
+#include <cstdio>
+#include <conio.h>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+struct Date {
+	int day;
+	int month;
+	int year;
+	int hour;
+	int minute;
+	int second;
+};
+
+class Time_is_now {
+public:
+	Time_is_now();
+	~Time_is_now();
+
+	void change_GMT();
+	void show_current_date();
+	Date get_current_date();
+private:
+	void update();
+	Date current_date;
+	int GMT;
+};
+
