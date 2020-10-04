506b81ec4fcfaf2167904b50f77edbe43458e571
KirillKotsko <kerchickd@gmail.com>
2020-10-04 13:41:08 +0300

    Add demonstraation mode for lab1_list1

--- lab1/lab1_list1_v3/lab1_list1_v3/Source.cpp
+++ lab1/lab1_list1_v3/lab1_list1_v3/Source.cpp
@@ -1,6 +1,7 @@
 #include "containers.h"
 #include "unit_test_framework.h"
 #include "test.h"
+#include "demonstration.h" 
 #include <iostream>
 
 using namespace std;
@@ -10,5 +11,9 @@ int main() {
 	RUN_TEST(tr, TestStack);
 	RUN_TEST(tr, TestQueue);
 	RUN_TEST(tr, TestDeque);
+	system("pause");
+	demonstration_stack();
+	demonstraation_queue(); 
+	demonstration_deque();
 	return 0;
 }
--- lab1/lab1_list1_v3/lab1_list1_v3/demonstration.h
+++ lab1/lab1_list1_v3/lab1_list1_v3/demonstration.h
@@ -1,1 +1,14 @@
 #pragma once
+#include "containers.h"
+#include "windows.h"
+#include <cstdlib>
+#include <vector>
+
+template <typename T>
+ostream& operator<<(ostream& os, const vector<T>& a);
+
+void demonstration_stack();
+
+void demonstraation_queue();
+
+void demonstration_deque();
