864ce77ae6b9fc35a9e681e05bd3949fb3d396ac
KirillKotsko <kerchickd@gmail.com>
2020-11-28 14:01:21 +0200

    Class FunctionGraph

--- lab3/Graphics/FunctionGraph/functiongraph.h
+++ lab3/Graphics/FunctionGraph/functiongraph.h
@@ -1,11 +1,24 @@
 #ifndef FUNCTIONGRAPH_H
 #define FUNCTIONGRAPH_H
 
+#include <cmath>
 
 class FunctionGraph
 {
 public:
-    FunctionGraph();
+    FunctionGraph(double _a = 0, double _b = 0, double _c = 0, int _function_id = 0) :
+        a(_a), b(_b), c(_c), function_id(_function_id) {}
+
+    void setA(double _a);
+    void setB(double _b);
+    void setC(double _c);
+    void setFunctionId(int _function_id);
+
+    double EvaluateFunc(double x);
+
+private:
+    double a,b,c;
+    int function_id;
 };
 
 #endif // FUNCTIONGRAPH_H
